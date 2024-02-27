0011bf0fe9ffadedeeeb7d455511d06530aa8282
MURILO CAROLINO DOS SANTOS <23111821@CFP127ALUNO.LOCAL>
2024-02-27 13:51:47 -0300

    Projeto em andamento

--- mais.js
+++ mais.js
@@ -6,5 +6,7 @@ quadrado.addEventListener('mouseover', function(){
     console.log('aaaaaaaaaaa')
     adicionar.classList.add('padding')
 
-    
+    document.getElementById("adicionar").addEventListener("click", function() {
+        window.location.href = "/tarefas.html";
+    });
 })
